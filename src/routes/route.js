8d7f3028a7f8fdfcf3763079c84cf93f798f075f
bhawna23999 <bhavnaagrawal1999@gmail.com>
2022-04-06 19:03:32 +0530

    node module practice

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const blogger = require('./logger')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    console.log("my name is bhawna")
+    console.log('the endpoint value is', blogger.url)
+    console.log('calling log function')
+    blogger.logging()
     res.send('My first ever api!')
 });
 
