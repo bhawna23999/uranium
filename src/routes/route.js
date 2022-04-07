79c6962a6db58717a65c68f786e55d1d4a8b8ac9
bhawna23999 <bhavnaagrawal1999@gmail.com>
2022-04-07 23:16:30 +0530

    query api practice

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,34 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/all-candidates', function (req, res) {
+    // console.log('------------------')
+    // console.log(req)
+    // console.log('------------------')
+    // console.log('These are the request query parameters: ', req.query)
+
+    let a = ["bhawna","ankita","hemant","gagan","parul","cp","omansh","deeksha","dinesh","sheela"]
+
+
+    res.send(a)
+});
+
+router.get('/candidates',function(req,res)
+{
+
+    
+    
+    let arr = ["bhawna","ankita","hemant","gagan","parul","cp","omansh","deeksha","dinesh","sheela"]
+    const arrNew = [];
+    for (let index = 0; index < req.query.count; index++) 
+    {
+        arrNew.push(arr[index]);          
+    }
+    
+   
+    res.send(arrNew);
+
+
 });
 
 
