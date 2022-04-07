10516e976c2386bf4bb625ac4f3460f4da44f064
bhawna23999 <bhavnaagrawal1999@gmail.com>
2022-04-08 02:57:38 +0530

    path-parameter

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,96 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
-})
+router.get('/movies',function(req,res)
+{
+    let arr = ["good will hunting","lean on me","the paper chase","forrest gump","theory of everyhting"]
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    res.send(arr)
 });
 
+router.get('/movies/:indexNumber', function(req,res)
+{
+    let a;
+    let arr = ["good will hunting","lean on me","the paper chase","forrest gump","theory of everyhting"]
+    for(let i=0; i<arr.length; i++)
+    {
+        if(req.params.indexNumber == i)
+        {
+            a = arr[i]
 
+        }
+    }
+    if(req.params.indexNumber>arr.length)
+    {
+        a = " use a valid index."
+    }
+    res.send(a)
 
+});
+
+router.get('/films',function(req,res)
+{
+    let arr = [ 
+        {       
+        "id": 1,
+        "name": "The Shining"
+        }, 
+        {
+        "id": 2,
+        "name": "Incendies"
+        },
+        {
+        "id": 3,
+        "name": "Rang de Basanti"
+        }, 
+        {
+        "id": 4,
+        "name": "Finding Nemo"
+        }] 
+        res.send(arr)      
+});
+
+router.get('/films/:filmId',function(req,res)
+{
+    let arr = [ 
+        {       
+        "id": 1,
+        "name": "The Shining"
+        }, 
+        {
+        "id": 2,
+        "name": "Incendies"
+        },
+        {
+        "id": 3,
+        "name": "Rang de Basanti"
+        }, 
+        {
+        "id": 4,
+        "name": "Finding Nemo"
+        }] 
+
+        let a;
+        for(i=0; i<arr.length; i++)
+        {
+            // console.log(arr[i].id)
+            if(req.params.filmId == arr[i].id)
+            {
+                a = arr[i]
+                
+            }
+            
+        }
+        if(req.params.filmId>arr.length)
+        {
+            a =  "No movie exists with this id"
+        }
+        res.send(a)
+
+})
 
 module.exports = router;
 // adding this comment for no reason
